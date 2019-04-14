d547e97ec68e54521ed0e9c8e1b80c167abfc430
Robbert Segers <robbert.segers@klarrio.com>
2019-04-14 21:31:44 +0200

    split data to separate files, setup income share data

---
+++ src/app/home/data/incomeGrowth.ts
@@ -0,0 +1,62 @@
+// http://gabriel-zucman.eu/usdina/
+
+const dataSet_1946_1980 = [
+  {
+    label: 'Pretax income growth (1946–1980)',
+    backgroundColor: 'lightgreen',
+    borderColor: 'green',
+    borderWidth: 1,
+    data: [109, 101, 105, 79, 47, 54, 76, 57]
+  },
+  {
+    label: 'Posttax income growth (1946–1980)',
+    backgroundColor: 'lightblue',
+    borderColor: 'blue',
+    borderWidth: 1,
+    data: [179, 117, 98, 69, 58, 104, 201, 163]
+  }
+];
+const dataSet_1980_2014 = [
+  {
+    label: 'Pretax income growth (1980–2014)',
+    backgroundColor: 'lightgreen',
+    borderColor: 'green',
+    borderWidth: 1,
+    data: [-25, 7, 42, 121, 204, 320, 453, 636]
+  },
+  {
+    label: 'Posttax income growth (1980–2014)',
+    backgroundColor: 'lightblue',
+    borderColor: 'blue',
+    borderWidth: 1,
+    data: [4, 26, 49, 113, 194, 298, 423, 616]
+  }
+];
+
+const barChartData = {
+  labels: ['p0 - p20', 'p20 - p50', 'p50 - p90', 'p90 - p100', 'top 1%', 'top 0.1%', 'top 0.01%', 'top 0.001%'],
+  datasets: dataSet_1946_1980
+};
+
+const chartOptions = {
+  responsive: true,
+  legend: { position: 'top' },
+  title: {
+    display: true,
+    text: 'The Distribution of Economic Growth in the United States post WWII'
+  },
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  }
+};
+
+export {
+  dataSet_1946_1980,
+  dataSet_1980_2014,
+  barChartData,
+  chartOptions
+};
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,15 @@ import {
   Component,
   AfterViewInit
 } from '@angular/core';
-
 import Chart from 'chart.js';
+
+import {
+  dataSet_1946_1980,
+  dataSet_1980_2014,
+  barChartData as distEcoGrowthData,
+  chartOptions as distEcoGrowthOptions
+} from './data/incomeGrowth';
+import { incomeShare } from './data/incomeShare';
 
 @Component({
   selector: 'app-home',
@@ -12,77 +19,32 @@ import Chart from 'chart.js';
 })
 export class HomeComponent implements AfterViewInit {
 
+  distEcoGrowthChart: Chart;
+  incomeShareChart: Chart;
+
   constructor() { }
 
   ngAfterViewInit() {
 
-    const barChartData = {
-      labels: [
-        "p0 - p20",
-        "p20 - p50",
-        "p50 - p90",
-        "p90 - p100",
-        "top 1%",
-        "top 0.1%",
-        "top 0.01%",
-        "top 0.001%"
-      ],
-      datasets: [
-        {
-          label: "Pretax income growth (1946–1980)",
-          backgroundColor: "orange",
-          borderColor: "red",
-          borderWidth: 1,
-          data: [109, 101, 105, 79, 47, 54, 76, 57]
-        },
-        {
-          label: "Pretax income growth (1980–2014)",
-          backgroundColor: "lightgreen",
-          borderColor: "green",
-          borderWidth: 1,
-          data: [-25, 7, 42, 121, 204, 320, 453, 636]
-        },
-        {
-          label: "Posttax income growth (1946–1980)",
-          backgroundColor: "pink",
-          borderColor: "purple",
-          borderWidth: 1,
-          data: [179, 117, 98, 69, 58, 104, 201, 163]
-        },
-        {
-          label: "Posttax income growth (1980–2014)",
-          backgroundColor: "lightblue",
-          borderColor: "blue",
-          borderWidth: 1,
-          data: [4, 26, 49, 113, 194, 298, 423, 616]
-        }
-      ]
-    };
-
-    const chartOptions = {
-      responsive: true,
-      legend: {
-        position: "top"
-      },
-      title: {
-        display: true,
-        text: "The Distribution of Economic Growth in the United States post WWII"
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    };
-
     const ctx = document.getElementById('homeChart');
-    const myChart = new Chart(ctx, {
+    this.distEcoGrowthChart = new Chart(ctx, {
       type: 'bar',
-      data: barChartData,
-      options: chartOptions
+      data: distEcoGrowthData,
+      options: distEcoGrowthOptions
+    });
+    this.incomeShareChart = new Chart(ctx, {
+      type: 'line',
+      data: incomeShare,
+      options: distEcoGrowthOptions
     });
   }
 
+  toggleEcoGrowthTime() {
+    if (this.distEcoGrowthChart.data.datasets === dataSet_1980_2014) {
+      this.distEcoGrowthChart.data.datasets = dataSet_1946_1980;
+    } else {
+      this.distEcoGrowthChart.data.datasets = dataSet_1980_2014;
+    }
+    this.distEcoGrowthChart.update();
+  }
 }
