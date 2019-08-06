76596b2771c6a9230047752a26d98d2cfcea09a0
Robbert Segers <robbert.segers@hotmail.com>
2019-08-06 16:58:07 +0200

    add & setup rellax parallax lib

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,28 @@
 import {
   Component,
+  AfterViewInit,
 } from '@angular/core';
+import * as Rellax from 'rellax';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor() {
+    console.log('https://github.com/dixonandmoe/rellax');
+  }
+
+  ngAfterViewInit() {
+
+    new Rellax('.rellax', {
+      speed: -2,
+      vertical: false,
+      horizontal: true
+    });
+
+  }
 
 }
