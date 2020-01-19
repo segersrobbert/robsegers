26b390e4b434d6150c0ff2d15fa504ff82f54319
segersrobbert <robbert.segers@hotmail.com>
2020-01-19 22:45:52 +0100

    cleanup

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/robsegers'));
+app.use(
+  express.static(__dirname + '/dist/robsegers')
+);
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/robsegers/index.html'));
+app.get('/*', (req,res) => {
+  res.sendFile(
+    path.join(`${__dirname}/dist/robsegers/index.html`)
+  );
 });
 
-// Start the app by listening on the default Heroku port
+// Start & listen on the default Heroku port
 app.listen(process.env.PORT || 8080);
